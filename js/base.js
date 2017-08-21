f813dfc2e0072ba31f3169969322a3707872c137
Chris Chafin <chris182@verizon.net>
2017-08-21 10:28:34 -0600

    Link Changes
    Changes to Codepen links and some editing

--- js/base.js
+++ js/base.js
@@ -2,9 +2,10 @@
 $('body').scrollspy({target: '.navbar-fixed-top'})
 
 // jQuery for page scrolling feature - requires jQuery Easing plugin
-$(function() {$('body').on('click', 'a.scrollable', function(event) {
-  var $anchor = $(this);
-  $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top},1500,'easeInOutExpo');
-  event.preventDefault();
-});
-});
+// $(function() {
+//   $('body').on('click', 'a.scrollable', function(event) {
+//     var $anchor = $(this);
+//     $('html, body').stop().animate({scrollTop: $($anchor.attr('href')).offset().top},1500,'easeInOutExpo');
+//     event.preventDefault();
+//   });
+// });
